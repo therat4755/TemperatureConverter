e5486f2744bc094183ba5676c46ce56d6b9891cc
Bohdan <122364847+therat4755@users.noreply.github.com>
2026-07-01 12:20:19 +0300

    the design of the program has been improved

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from convert import celsius_to_fahrenheit
 
 celsius = int(input("Enter the temperature in Celsius: "))
 
-print(celsius_to_fahrenheit(celsius))
+print(f'Celsius: {celsius}\nFahrenheit: {celsius_to_fahrenheit(celsius)}')
